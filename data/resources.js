62584193b27ce39ed40abdde8232d2ff768737b7
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2023-06-20 09:52:11 -0400

    First Commit

---
+++ data/resources.js
@@ -0,0 +1,23 @@
+export const resources = [
+  {
+    image: "elonmusk.jpg",
+    name: "Game Development",
+    description:
+      "Ryan this is your time to shine",
+    link: "https://docs.google.com/",
+  },
+  {
+    image: "elonmusk.jpg",
+    name: "ACSL",
+    description:
+      "Basic Lessons on ACSL",
+    link: "https://docs.google.com/",
+  },
+  {
+    image: "elonmusk.jpg",
+    name: "Web Dev",
+    description:
+      "React.js, HTML and CSS",
+    link: "https://docs.google.com/",
+  }
+]
