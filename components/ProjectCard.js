ae1770dc2b32f79b019470de0a1249a9d52b0b27
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2023-09-10 15:33:08 -0400

    Update ProjectCard.js

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -13,6 +13,7 @@ export default function ProjectCard({ title, link, bg }) {
       />
       <a
         className="rounded-md w-full h-full flex items-center justify-center transition ease-in-out duration-500 opacity-0 hover:bg-black/60 hover:opacity-100 z-10"
+        href={link}
         target="_blank"
         rel="noopener noreferrer"
       >
